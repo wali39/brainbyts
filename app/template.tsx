f9bbf03b5dbe9075d478f8e3d761381a72c3829a
Wali Ullah <wali190939@gmail.com>
2024-09-28 03:49:26 +0600

    add: add template for adding page transition

---
+++ app/template.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import Navbar from "@/components/navbar";
+import Provider from "@/context/provider";
+import { AntdRegistry } from "@ant-design/nextjs-registry";
+import { Toaster } from "react-hot-toast";
+import { motion } from "framer-motion";
+import { usePathname } from "next/navigation";
+
+export default function Template({ children }: { children: React.ReactNode }) {
+  const variants = {
+    hidden: { opacity: 0, y: -5 },
+    enter: { opacity: 1, y: 0 },
+    exit: { opacity: 0, y: 0 },
+  };
+  const key = usePathname();
+  return (
+    <div>
+      <Provider session={""}>
+        <AntdRegistry>
+          <Navbar />
+
+          <motion.div
+            key={key}
+            initial="hidden"
+            animate="enter"
+            exit="exit"
+            variants={variants}
+            transition={{ ease: "easeInOut", duration: 0.5, delay: 0.25 }}
+          >
+            {children}
+          </motion.div>
+          <Toaster />
+        </AntdRegistry>
+      </Provider>
+    </div>
+  );
+}
