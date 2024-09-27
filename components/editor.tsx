2ee4d75c807f583acff774cc418f618f82aac0b6
Wali Ullah <wali190939@gmail.com>
2024-09-28 04:02:23 +0600

    add: react-quill editor and display added

--- components/editor.tsx
+++ components/editor.tsx
@@ -1,30 +1,40 @@
+"use client";
 import React, { useMemo } from "react";
 import dynamic from "next/dynamic";
 import "react-quill/dist/quill.snow.css";
+import { Quill } from "react-quill";
 
 interface EditorProps {
   onChange: (value: string) => void;
   value: string;
 }
-
+const Font = Quill.import("formats/font");
+Font.whitelist = ["roboto"];
+Quill.register(Font, true);
 const Editor = ({ onChange, value }: EditorProps) => {
   const modules = {
     toolbar: [
-      [{ header: [1, 2, false] }],
+      [{ font: [] }],
+      [{ size: ["small", false, "large", "huge"] }],
       ["bold", "italic", "underline", "strike", "blockquote"],
       [
         { list: "ordered" },
         { list: "bullet" },
+        { list: "check" },
         { indent: "-1" },
         { indent: "+1" },
+        { align: [] },
       ],
       ["link", "image"],
+      ["code"],
       ["clean"],
     ],
   };
 
   const formats = [
     "header",
+    "font",
+    "size",
     "bold",
     "italic",
     "underline",
@@ -33,6 +43,7 @@ const Editor = ({ onChange, value }: EditorProps) => {
     "list",
     "bullet",
     "indent",
+    "code",
     "link",
     "image",
   ];
--- components/preview.tsx
+++ components/preview.tsx
@@ -1,7 +1,9 @@
+"use client";
 import dynamic from "next/dynamic";
 import React, { useMemo } from "react";
+import ReactQuill from "react-quill";
 
-import "react-quill/dist/quill.snow.css";
+import "react-quill/dist/quill.bubble.css";
 interface PreviewProps {
   value: string;
 }
@@ -12,6 +14,13 @@ const Preview = ({ value }: PreviewProps) => {
     []
   );
 
-  return <ReactQuill theme="bubble" value={value} readOnly />;
+  return (
+    <ReactQuill
+      theme="bubble"
+      value={value}
+      readOnly
+      // className="line-clamp-2 overflow-hidden h-20"
+    />
+  );
 };
 export default Preview;
