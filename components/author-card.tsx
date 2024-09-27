f5fbd7eb5b0ff961a806596bc21b6089f06d22df
Wali Ullah <wali190939@gmail.com>
2024-09-28 04:01:13 +0600

    add: blog-card and author card

--- components/author-card.tsx
+++ components/author-card.tsx
@@ -1,16 +1,19 @@
+"use client";
 import Image, { StaticImageData } from "next/image";
-import { FaArrowUp } from "react-icons/fa";
+import Link from "next/link";
+import { FaArrowUp, FaUserAstronaut } from "react-icons/fa";
+
 interface AuthorCardProps {
   id: string;
-  imgsrc: StaticImageData;
-  name: string;
+  imageUrl?: string | null;
+  name: string | null;
   description: string;
   IsauthorPage?: boolean;
   publishedPosts?: number;
 }
 const AuthorCard = ({
   id,
-  imgsrc,
+  imageUrl,
   name,
   description,
   IsauthorPage = false,
@@ -18,38 +21,51 @@ const AuthorCard = ({
 }: AuthorCardProps) => {
   return (
     <div
-      className={`px-2 text-center md:text-start  max-w-screen-md    w-full md:flex  overflow-hidden rounded-xl items-center space-x-2  ${
-        IsauthorPage ? "mx-auto" : "border-2"
+      className={`px-2 text-center md:text-start w-full  max-w-screen-sm   md:flex   overflow-hidden rounded-xl items-center space-x-2  ${
+        IsauthorPage ? "mx-auto border-2 border-primary p-3" : "shadow-md"
       }`}
     >
-      <Image
-        src={imgsrc}
-        alt=""
-        className={`object-cover w-24 h-24 md:w-36 md:h-36 aspect-square p-2  mx-auto ${
-          IsauthorPage ? "rounded-3xl" : "rounded-full"
-        }`}
-      />
+      {imageUrl ? (
+        <Image
+          src={imageUrl}
+          height={200}
+          width={200}
+          alt=""
+          className={`object-cover w-24 h-24 md:w-28 md:h-28 aspect-square p-2    ${
+            IsauthorPage ? "rounded-3xl" : "rounded-2xl"
+          }`}
+        />
+      ) : (
+        <div
+          className={` w-24 h-24 md:w-36 md:h-36 aspect-square p-2  mx-auto  flex items-center justify-center  text-[#8b1a1aab]  `}
+        >
+          <FaUserAstronaut size={70} />
+        </div>
+      )}
       <div>
         {publishedPosts && (
           <p className="text-base font-normal md:text-md ">
-            <span className="font-bold ">05</span> Published Posts
+            <span className="font-bold ">
+              {publishedPosts < 10 ? `0${publishedPosts}` : publishedPosts}
+            </span>{" "}
+            Published Posts
           </p>
         )}
         <p className="text-lg md:text-2xl font-medium">{name}</p>
         <p
-          className={` text-sm md:text-base ${!IsauthorPage && "line-clamp-2"}`}
+          className={` text-sm md:text-base ${!IsauthorPage && "line-clamp-1"}`}
         >
           {description}
         </p>
-        <a
+        <Link
           className={`underline flex items-center text-sm  md:text-base hover:text-primary justify-center md:justify-start ${
             IsauthorPage && "hidden"
           }`}
-          href=""
+          href={`/blogs/author/${id}`}
         >
           See all post by this author
           <FaArrowUp className="rotate-45" />
-        </a>
+        </Link>
       </div>
     </div>
   );
--- components/blog-card.tsx
+++ components/blog-card.tsx
@@ -1,81 +1,160 @@
-import { ArrowRightOutlined, CalendarOutlined } from "@ant-design/icons";
-import Image, { StaticImageData } from "next/image";
-import blogImage from "/public/blogimage.jpg";
+"use client";
+import Image from "next/image";
+import Link from "next/link";
+import readingTime from "reading-time";
+import {
+  ArrowRightOutlined,
+  CalendarOutlined,
+  EditOutlined,
+  UserOutlined,
+} from "@ant-design/icons";
+import Preview from "./preview";
+import { FaRegImage } from "react-icons/fa";
+import { useSession } from "next-auth/react";
+import { staticGenerationAsyncStorage } from "next/dist/client/components/static-generation-async-storage-instance";
 interface BlogCardProps {
-  blogImg: StaticImageData;
-  category: string;
-  title: string;
-  description: string;
+  blogData: {
+    id: string;
+    author: {
+      id: string;
+      name: string | null;
+      imageUrl?: string | null;
+    };
+    title: string;
+    description: string | null;
+    imageUrl: string | undefined;
+    categoryId?: string;
+    category?: string;
+    createdAt: Date;
+  };
 }
-const BlogCard = ({ blogImg, category, title, description }: BlogCardProps) => {
+const BlogCard = ({ blogData }: BlogCardProps) => {
+  const {
+    id,
+    title,
+    description,
+    imageUrl: blogImage,
+    category,
+    categoryId,
+    createdAt,
+  } = blogData;
+  const {
+    id: authorId,
+    name: authorName,
+    imageUrl: authorImage,
+  } = blogData.author;
+  const { data: session } = useSession();
+  // console.log("reading time", readingTime(description || ""));
   return (
-    <div className="bg-[#f8f5f57c] p-4 rounded-lg cursor-pointer group relative shadow-md">
-      <div className="relative">
-        <span className="absolute inline-block right-0 top-0 rounded-bl-md rounded-tr-md pl-[0.5rem] pb-[0.5rem] bg-background ">
-          <p className="px-[1rem] py-[0.5rem] inline-block rounded-[0.5rem] border-[2px] uppercase hover:bg-accent hover:text-white">
-            {category}
-          </p>
+    <Link href={`/blogs/details/${id}`}>
+      <div className="bg-[#f8f5f57c] p-4 rounded-lg cursor-pointer group relative shadow-md z-10">
+        <div className="relative">
+          <span className="absolute inline-block right-0 top-0 rounded-bl-md rounded-tr-md pl-[0.5rem] pb-[0.5rem] bg-background ">
+            <Link href={`/blogs/category/${categoryId}`}>
+              <p className="px-[1rem] py-[0.5rem] inline-block rounded-[0.5rem] border-[2px] uppercase hover:bg-accent hover:text-white">
+                {category}
+              </p>
+            </Link>
 
-          <div className="text-background border-0  top-0 -left-[0.005rem] sm:-left-[0.75rem]  rotate-90 md:left-[0.05rem] absolute">
-            <svg
-              className="absolute h-[0.75rem] w-[0.75rem] block align-middle"
-              width="101"
-              height="101"
-              viewBox="0 0 101 101"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fill-rule="evenodd"
-                clip-rule="evenodd"
-                d="M101 0H0V101H1C1 45.7715 45.7715 1 101 1V0Z"
-                fill="currentColor"
-              ></path>
-            </svg>
-          </div>
-          <div className="text-background  bottom-0 -right-[0.005rem]  rotate-90 md:-right-[0.005rem] absolute">
-            <svg
-              className="absolute h-[0.75rem] w-[0.75rem] block align-middle"
-              width="101"
-              height="101"
-              viewBox="0 0 101 101"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fill-rule="evenodd"
-                clip-rule="evenodd"
-                d="M101 0H0V101H1C1 45.7715 45.7715 1 101 1V0Z"
-                fill="currentColor"
-              ></path>
-            </svg>
-          </div>
-        </span>
+            <div className="text-background border-0  top-0 -left-[0.005rem] sm:-left-[0.75rem]  rotate-90 md:left-[0.05rem] absolute">
+              <svg
+                className="absolute h-[0.75rem] w-[0.75rem] block align-middle"
+                width="101"
+                height="101"
+                viewBox="0 0 101 101"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill-rule="evenodd"
+                  clip-rule="evenodd"
+                  d="M101 0H0V101H1C1 45.7715 45.7715 1 101 1V0Z"
+                  fill="currentColor"
+                ></path>
+              </svg>
+            </div>
+            <div className="text-background  bottom-0 -right-[0.005rem]  rotate-90 md:-right-[0.005rem] absolute">
+              <svg
+                className="absolute h-[0.75rem] w-[0.75rem] block align-middle"
+                width="101"
+                height="101"
+                viewBox="0 0 101 101"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill-rule="evenodd"
+                  clip-rule="evenodd"
+                  d="M101 0H0V101H1C1 45.7715 45.7715 1 101 1V0Z"
+                  fill="currentColor"
+                ></path>
+              </svg>
+            </div>
+          </span>
 
-        <Image
-          className="rounded-xl md:rounded-2xl  bg-white/40 w-full object-cover h-60 "
-          src={blogImg}
-          alt=""
-        />
-      </div>
-      <div className="">
-        <div className="px-2 mt-3">
-          <p className="text-2xl font-normal hover:text-primary  ">{title}</p>
-          <p className="py-3 space-x-2 font-medium text-md">
-            <CalendarOutlined className="text-[16px] " />
-            <span>01 Jan, 2024</span>
-          </p>
-          <p className="line-clamp-2 font-normal text-lg ">{description}</p>
+          {blogImage ? (
+            <Image
+              className="rounded-xl md:rounded-2xl  bg-white/40 w-full object-cover h-60 "
+              src={blogImage}
+              height={200}
+              width={200}
+              alt=""
+            />
+          ) : (
+            <div className="max-w-xl h-60 object-cover bg-stone-200 rounded-xl flex items-center justify-center text-accent">
+              <FaRegImage size={30} />
+            </div>
+          )}
         </div>
-        <div className="flex space-x-3 items-center font-medium text-md mt-5 px-2 text-slate-800">
-          <Image className="rounded-md w-8 h-8" src={blogImage} alt="" />
-          <p>Wali Ullah</p>
-          <span className="w-2 h-2 bg-accent rounded-full" />
-          <span>01 minute read</span>
+        <div>
+          <div className="px-2 mt-3">
+            <p className="text-2xl font-normal hover:text-primary  ">{title}</p>
+            <p className="py-3 space-x-2 font-medium text-md">
+              <CalendarOutlined className="text-[16px] " />
+              <span>{createdAt.toDateString()}</span>
+            </p>
+            {/* <p className="line-clamp-2 font-normal text-lg "> */}
+            {/* <Preview value={description || ""} /> */}
+            <div className="ql-snow">
+              <div
+                className=" line-clamp-3 text-lg "
+                dangerouslySetInnerHTML={{ __html: description || "" }}
+              />
+            </div>
+          </div>
+          <div className="flex space-x-3 items-center font-medium text-md mt-5 px-2 text-slate-800">
+            {authorImage ? (
+              <Image
+                className="rounded-md w-8 h-8"
+                height={200}
+                width={200}
+                src={authorImage}
+                alt=""
+              />
+            ) : (
+              <div className="w-8 h-8 rounded">
+                <UserOutlined />
+              </div>
+            )}
+            <Link
+              href={`/blogs/author/${authorId}`}
+              className=" hover:text-accent"
+            >
+              <p>{authorName}</p>
+            </Link>
+            <span className="w-2 h-2 bg-accent rounded-full" />
+            <span>{readingTime(description || "").text}</span>
+            {(session?.user?.id === authorId ||
+              session?.user?.role == "admin") && (
+              <Link href={`/blogs/${authorId}/${id}`}>
+                <EditOutlined /> Edit
+              </Link>
+            )}
+          </div>
+          <ArrowRightOutlined className=" font-bold text-xl bg-accent  text-white rounded-full p-2 absolute right-5 bottom-5 hover:-rotate-45 " />
         </div>
-        <ArrowRightOutlined className=" font-bold text-xl bg-accent -rotate-45 text-white rounded-full p-2 absolute right-5 bottom-5 hover:rotate-45 " />
       </div>
-    </div>
+    </Link>
   );
 };
 
