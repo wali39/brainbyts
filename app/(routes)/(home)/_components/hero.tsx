125de7ff85ee8fbe0179e90e5a930e17523eb672
Wali Ullah <wali190939@gmail.com>
2024-09-28 03:50:09 +0600

    refractor: hero updated

---
+++ app/(routes)/(home)/_components/hero.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+const Hero = () => {
+  return (
+    <div className="pt-[10%] pb-[5%]">
+      <div className="text-xl  p-[3rem]   rounded-lg bg-cardBg shadow-sm   sm:text-3xl md:text-4xl md:font-serif text-[#1d070bbe] text-center  mx-auto  md:w-2/3  ">
+        <p className="hero ">
+          Taking control of your daily life is easy when you know how !
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
