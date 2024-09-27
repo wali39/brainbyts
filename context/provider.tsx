55d6381488e233f3d284f6f1b3882bae8beed25f
Wali Ullah <wali190939@gmail.com>
2024-09-28 04:03:32 +0600

    next-auth provider updated

--- context/provider.tsx
+++ context/provider.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { SessionProvider } from "next-auth/react";
+import { useEffect, useState } from "react";
 
 const Provider = ({
   children,
@@ -9,6 +10,13 @@ const Provider = ({
   children: React.ReactNode;
   session: any;
 }) => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) return null;
   return <SessionProvider session={session}>{children}</SessionProvider>;
 };
 
